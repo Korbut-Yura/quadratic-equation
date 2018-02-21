85414189250dac18ccba5021941d19b24bd42d16
Yura-Korbut <korbuty@mail.ru>
2018-02-22 00:59:03 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let result = [];
+  equation = equation.replace(/\s/g, '');
+  let matches = equation.match(/-?\d+(\.\d+)?/g);
+  let a = +matches[0],
+      b = +matches[2],
+      c = +matches[3];
+
+  let disc = Math.pow(b,2)-4*a*c;
+  if (disc > 0) {
+    result[0] = Math.round((-b+Math.sqrt(disc))/2/a);
+    result[1] = Math.round((-b-Math.sqrt(disc))/2/a); 
+  }
+  return result.sort(function(a,b) {
+    return a-b;
+  });
 }
